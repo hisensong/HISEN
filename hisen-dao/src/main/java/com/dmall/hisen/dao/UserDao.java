f0a8aae8457806039e8a9668f07c7989618ee227
hisensong <HisenSong@163.com>
2016-08-13 22:40:52 +0800

    添加基础包

---
+++ hisen-dao/src/main/java/com/dmall/hisen/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.dmall.hisen.dao;
+
+/**
+ * Created by hisensong on 2016/8/13.
+ */
+public class UserDao {
+}
---
+++ hisen-dao/src/main/java/com/dmall/hisen/dao/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.dmall.hisen.dao.impl;
+
+/**
+ * Created by hisenson on 2016/8/13.
+ */
+public class UserDaoImpl {
+}
