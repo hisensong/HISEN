e4fc82791c3c8b02516f1cbcf1eba4c978d2739e
HisenSong <songhaizhen@gomeholdings>
2016-08-23 17:42:28 +0800

    添加公共模块

---
+++ hisen-common/src/main/java/com/dmall/hisen/utils/DateUtils.java
@@ -0,0 +1,87 @@
+package com.dmall.hisen.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Dmall on 15/12/9.
+ */
+public class DateUtils {
+
+    /**
+     * 计算两个日期之间相差的天数
+     * @param smdate 较小的时间
+     * @param bdate  较大的时间
+     * @return 相差天数
+     * @throws Exception
+     */
+    public static int daysBetween(Date smdate, Date bdate) throws Exception
+    {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        smdate=sdf.parse(sdf.format(smdate));
+        bdate=sdf.parse(sdf.format(bdate));
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(smdate);
+        long time1 = cal.getTimeInMillis();
+        cal.setTime(bdate);
+        long time2 = cal.getTimeInMillis();
+        long between_days=(time2-time1)/(1000*3600*24);
+
+        return Integer.parseInt(String.valueOf(between_days));
+    }
+
+    /**
+     *字符串的日期格式的计算
+     */
+    public static int daysBetween(String smdate,String bdate) throws Exception{
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(sdf.parse(smdate));
+        long time1 = cal.getTimeInMillis();
+        cal.setTime(sdf.parse(bdate));
+        long time2 = cal.getTimeInMillis();
+        long between_days=(time2-time1)/(1000*3600*24);
+
+        return Integer.parseInt(String.valueOf(between_days));
+    }
+
+    /**
+     * 增加一天
+     * @param s
+     * @param n
+     * @return
+     */
+    public static String addDay(String s, int n) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+            Calendar cd = Calendar.getInstance();
+            cd.setTime(sdf.parse(s));
+            cd.add(Calendar.DATE, n);//增加一天
+            //cd.add(Calendar.MONTH, n);//增加一个月
+
+            return sdf.format(cd.getTime());
+
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    public static String addDayTime(String s, int n) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            Calendar cd = Calendar.getInstance();
+            cd.setTime(sdf.parse(s));
+            cd.add(Calendar.DATE, n);//增加一天
+            return sdf.format(cd.getTime());
+
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+}
