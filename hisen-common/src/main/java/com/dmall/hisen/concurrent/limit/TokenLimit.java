dacf830de1604659441834b77cfb8aa2476cf110
hisensong <HisenSong@163.com>
2016-08-31 23:58:58 +0800

    添加谷歌令牌桶算法

--- hisen-common/src/main/java/com/dmall/hisen/concurrent/limit/TokenLimit.java
+++ hisen-common/src/main/java/com/dmall/hisen/concurrent/limit/TokenLimit.java
@@ -23,14 +23,14 @@ public class TokenLimit {
      * @throws InterruptedException
      */
     public static void smoothBursty() throws InterruptedException {
-        RateLimiter limiter = RateLimiter.create(5, 1000, TimeUnit.MILLISECONDS);
-        for(int i = 1; i < 5;i++) {
-            System.out.println(limiter.acquire());
-        }
-        Thread.sleep(1000L);
-        for(int i = 1; i < 10;i++) {
-            System.out.println(limiter.acquire());
-        }
+
+        RateLimiter limiter = RateLimiter.create(5);
+        System.out.println(limiter.acquire());
+        System.out.println(limiter.acquire());
+        System.out.println(limiter.acquire());
+        System.out.println(limiter.acquire());
+        System.out.println(limiter.acquire());
+        System.out.println(limiter.acquire());
 
     }
 
@@ -39,12 +39,17 @@ public class TokenLimit {
      * @throws InterruptedException
      */
     public static void smoothWarmingUp() throws InterruptedException {
-        RateLimiter limiter = RateLimiter.create(50, 1000, TimeUnit.MILLISECONDS);
+        /**
+         * RateLimiter.create(doublepermitsPerSecond, long warmupPeriod, TimeUnit unit)
+         *permitsPerSecond表示每秒新增的令牌数，warmupPeriod表示在从冷启动速率过渡到平均速率的时间间隔。
+         */
+        RateLimiter limiter = RateLimiter.create(5, 1000, TimeUnit.MILLISECONDS);
         for(int i = 1; i < 5;i++) {
+            Thread.sleep(1000L);
             System.out.println("limiter.tryAcquire()==="+limiter.tryAcquire());
             System.out.println(limiter.acquire());
         }
-        Thread.sleep(1000L);
+
         for(int i = 1; i < 10;i++) {
             System.out.println(limiter.acquire());
 
