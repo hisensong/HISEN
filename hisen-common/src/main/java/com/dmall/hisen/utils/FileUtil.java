4287067f7911b1db4dce486faf2683430d9337c8
HisenSong <songhaizhen@gomeholdings>
2016-09-07 10:12:59 +0800

    新增文件操作类

---
+++ hisen-common/src/main/java/com/dmall/hisen/utils/FileUtil.java
@@ -0,0 +1,49 @@
+package com.dmall.hisen.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileUtil {
+	//读取文件
+	public static byte[] getContent(String filePath){  
+        File file = new File(filePath);  
+        long fileSize = file.length();  
+        if (fileSize > Integer.MAX_VALUE) {  
+            System.out.println("file too big...");  
+            return null;  
+        } 
+        FileInputStream fi = null;
+        byte[] buffer = null;
+        try{
+        	fi = new FileInputStream(file);  
+            buffer = new byte[(int) fileSize];  
+            int offset = 0;  
+            int numRead = 0;  
+            while (offset < buffer.length && (numRead = fi.read(buffer, offset, buffer.length - offset)) >= 0) {  
+                	offset += numRead;  
+            }  
+            // 确保所有数据均被读取  
+            if (offset != buffer.length) {  
+            	throw new IOException("Could not completely read file " + file.getName());  
+            }  
+            fi.close();  
+            
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+        return buffer;  
+    }
+	//写入文件
+	public static void createFile(String path, byte[] content){
+       try{
+    	   FileOutputStream fos = new FileOutputStream(path);
+           fos.write(content);
+           fos.close(); 
+       }catch(IOException e){
+    	   e.printStackTrace();
+       }
+        
+    }
+}
